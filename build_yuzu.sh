b29f449ab247c70bd0bda096d545ebf27f8e8960
shinra-electric <50119606+shinra-electric@users.noreply.github.com>
2024-01-03 17:51:28 +0000

    Alphabetise brew install list
    Alphabetise the brew install list so it's easier to identify stuff.

--- build_yuzu.sh
+++ build_yuzu.sh
@@ -26,7 +26,7 @@ CORES=$(sysctl -n hw.ncpu)
 cd "$HOME"
 
 # Install needed dependencies
-brew install autoconf enet inih cubeb fmt automake boost ccache ffmpeg glslang hidapi libtool libusb lz4 ninja nlohmann-json openssl pkg-config qt@6 sdl2 speexdsp zlib zstd molten-vk vulkan-loader
+brew install autoconf automake boost ccache cubeb enet ffmpeg fmt glslang hidapi inih libtool libusb llvm@17 lz4 molten-vk ninja nlohmann-json openssl pkg-config qt@6 sdl2 speexdsp vulkan-loader zlib zstd
 
 echo -e "${PURPLE}Cloning or updating Yuzu repository...${NC}"
 
