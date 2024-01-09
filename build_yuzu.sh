305778399ff088106c6941573e556cc953c432d2
shinra-electric <50119606+shinra-electric@users.noreply.github.com>
2024-01-09 23:59:52 +0100

    Add gettext to the dependencies
    Required for the nx_txdb fix on Sonoma and Apple Silicon

--- build_yuzu.sh
+++ build_yuzu.sh
@@ -26,7 +26,7 @@ CORES=$(sysctl -n hw.ncpu)
 cd "$HOME"
 
 # Install needed dependencies
-brew install autoconf automake boost ccache cubeb enet ffmpeg fmt glslang hidapi inih libtool libusb llvm@17 lz4 molten-vk ninja nlohmann-json openssl pkg-config qt@6 sdl2 speexdsp vulkan-loader zlib zstd
+brew install autoconf automake boost ccache cubeb enet ffmpeg fmt gettext glslang hidapi inih libtool libusb llvm@17 lz4 molten-vk ninja nlohmann-json openssl pkg-config qt@6 sdl2 speexdsp vulkan-loader zlib zstd
 
 echo -e "${PURPLE}Cloning or updating Yuzu repository...${NC}"
 
