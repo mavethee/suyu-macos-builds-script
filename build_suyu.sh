a9447a7b0aac25bc1544b180e27f4ac140665ad6
Séamus <shinra.electric@icloud.com>
2024-03-09 10:41:12 +0100

    Rebranding to Suyu

--- build_suyu.sh
+++ build_suyu.sh
@@ -87,7 +87,7 @@ cmake .. -GNinja \
     -DENABLE_QT6=ON \
     -DSUYU_USE_EXTERNAL_VULKAN_HEADERS=OFF
 
-echo -e "${PURPLE}Building yuzu...${NC}"
+echo -e "${PURPLE}Building suyu...${NC}"
 
 # Build Suyu using Ninja
 ninja
@@ -97,18 +97,18 @@ if [ $? -eq 0 ]; then
     echo -e "${GREEN}Build successful${NC}."
 
     # Remove existing Suyu.app if it exists in /Applications
-    if [ -d "/Applications/yuzu.app" ]; then
+    if [ -d "/Applications/Suyu.app" ]; then
         echo -e "${PURPLE}Removing existing Suyu.app in /Applications...${NC}"
-        rm -rf "/Applications/yuzu.app"
+        rm -rf "/Applications/Suyu.app"
     fi
 
     # Bundle dependencies and codesign
-    dylibbundler -of -cd -b -x bin/yuzu.app/Contents/MacOS/yuzu -d bin/yuzu.app/Contents/libs/
+    dylibbundler -of -cd -b -x bin/suyu.app/Contents/MacOS/suyu -d bin/suyu.app/Contents/libs/
     
-    echo -e "${PURPLE}Moving yuzu.app to /Applications...${NC}"
+    echo -e "${PURPLE}Moving Suyu.app to /Applications...${NC}"
 
     # Move Suyu.app to /Applications
-    mv bin/yuzu.app /Applications/yuzu.app
+    mv bin/suyu.app /Applications/Suyu.app
 
     echo -e "${PURPLE}Installation completed.${NC}"
 
